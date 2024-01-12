7c3e0c35872b1bf41d3b71ba311603dd0ae55399
unknown <julieworks2@hotmail.com>
2024-01-12 13:37:30 -0600

    Crud de sujetos terminado

--- FacturaFiscalApi/app/Http/Controllers/SujetoController.php
+++ FacturaFiscalApi/app/Http/Controllers/SujetoController.php
@@ -3,8 +3,146 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sujeto;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class SujetoController extends Controller
 {
-    //
+    public function index(){
+        $sujetos = Sujeto::orderBy('k_sujetos', 'ASC')->limit(10)->get();
+        return response()->json([
+            'message' => 'Lista de sujetos',
+            'data' => $sujetos,
+            'status' => '200'
+        ], 200);
+    }
+
+    public function create(Request $request){
+        $validator = Validator::make($request->all(), [
+            'k_empresa' => 'required',
+            'sujetos_nombre' => 'required',
+            'sujetos_alias' => 'required',
+            'sujetos_regimen' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'Error al validar datos',
+                'errors' => $validator->errors(),
+                'status' => '400'
+            ], 400);
+        }
+
+        $sujeto = new Sujeto();
+        $sujeto->fill($request->all());
+        if($sujeto->save()){
+            return response()->json([
+                'message' => 'Sujeto creado correctamente',
+                'data' => $sujeto,
+                'status' => '200'
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Error al crear sujeto',
+            'status' => '400'
+        ], 400);
+
+    }
+
+    public function show($id, Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'k_empresa' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'Error al validar datos',
+                'errors' => $validator->errors(),
+                'status' => '400'
+            ], 400);
+        }
+
+        
+        $sujeto = DB::table('sujetos')->where('k_sujetos', $id)->where('k_empresa', $request->k_empresa)->first();
+
+        if($sujeto){
+            return response()->json([
+                'message' => 'Sujeto encontrado',
+                'data' => $sujeto,
+                'status' => '200'
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Sujeto no encontrado',
+            'status' => '400'
+        ], 400);
+    }
+
+    public function update(Int $id, Request $request){
+        $validator = Validator::make($request->all(), [
+            'k_empresa' => 'required',
+            'sujetos_nombre' => 'required',
+            'sujetos_alias' => 'required',
+            'sujetos_regimen' => 'required',
+            'sujetos_telefono' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'Error al validar datos',
+                'errors' => $validator->errors(),
+                'status' => '400'
+            ], 400);
+        }
+
+        $sujeto = Sujeto::where('k_sujetos', $id)->where('k_empresa', $request->k_empresa)->first();
+
+        if ($sujeto) {
+            $sujeto->update($request->all());
+
+            return response()->json([
+                'message' => 'Sujeto actualizado correctamente',
+                'data' => $sujeto,
+                'status' => '200'
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Sujeto no encontrado',
+            'status' => '404'
+        ], 404);
+    }
+
+    public function delete(Int $id, Request $request){
+        $validator = Validator::make($request->all(), [
+            'k_empresa' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'Error al validar datos',
+                'errors' => $validator->errors(),
+                'status' => '400'
+            ], 400);
+        }
+
+        $sujeto = Sujeto::where('k_sujetos', $id)->where('k_empresa', $request->k_empresa)->first();
+
+        if ($sujeto) {
+            $sujeto->delete();
+
+            return response()->json([
+                'message' => 'Sujeto eliminado correctamente',
+                'data' => $sujeto,
+                'status' => '200'
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Sujeto no encontrado',
+            'status' => '404'
+        ], 404);
+    }
 }
--- FacturaFiscalApi/app/Models/Sujeto.php
+++ FacturaFiscalApi/app/Models/Sujeto.php
@@ -10,7 +10,7 @@ class Sujeto extends Model
     use HasFactory;
 
     protected $table = 'sujetos';
-
+    protected $primaryKey = 'k_sujetos';
     protected $fillable = ['k_empresa', 'k_sujetos', 'sujetos_nombre', 'sujetos_calle', 'sujetos_numero_ext', 'sujetos_numero_int', 'sujetos_colonia', 'sujetos_ciudad', 'sujetos_estado', 'sujetos_cp', 'sujetos_pais', 'sujetos_email', 'sujetos_rfc', 'sujetos_referencia', 'sujetos_alias', 'sujetos_cliente', 'sujetos_proveedor', 'sujetos_telefono', 'borrado', 'sujetos_regimen']; 
     public $timestamps = false;
 
