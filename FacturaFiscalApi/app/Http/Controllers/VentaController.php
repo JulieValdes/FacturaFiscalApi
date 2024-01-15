e8b72962c4bcc15070320803721f0880c2e40ae2
Julie Valdes <julieworks2@gmail.com>
2024-01-15 11:59:08 -0600

    Problemas de compatibilidad con Eloquent

--- FacturaFiscalApi/app/Http/Controllers/VentaController.php
+++ FacturaFiscalApi/app/Http/Controllers/VentaController.php
@@ -17,6 +17,8 @@ class VentaController extends Controller
         ], 200);
     }
 
+
+    //Problemas aumentar k_venta en base a k_empresa
     public function create(Request $request){
         $validator = Validator::make($request->all(), [
             'k_empresa' => 'required',
@@ -41,13 +43,13 @@ class VentaController extends Controller
             ], 400);
         }
 
+        
+        // Obtenemos el último k_venta dentro de la transacción
         $ultimoKVenta = Venta::where('k_empresa', $request->k_empresa)->max('k_venta');
 
-        // Si no hay ventas para la k_empresa dada, se asigna 1 como el valor inicial de k_venta
-        $nuevoKVenta = $ultimoKVenta ? $ultimoKVenta + 1 : 1;
-
+        // Creamos el nuevo objeto Venta
         $venta = new Venta();
-        $venta->k_venta = $nuevoKVenta;
+        $venta->k_venta = $ultimoKVenta ? $ultimoKVenta + 1 : 1; // Si es nulo, empezamos desde 1
         $venta->fill($request->all());
         if ($venta->save()) {
             return response()->json([
@@ -92,4 +94,88 @@ class VentaController extends Controller
         ], 404);
     }
 
+    //es un problema que k_venta no sea autoincrementable ya que hay repetidos para una empresa y no hay manera de saber cual es el que se va a modificar
+    public function update(Request $request, $id){
+
+        $validator = Validator::make($request->all(), [
+            'k_empresa' => 'required',
+            'k_sujeto' => 'required',
+            //'venta_fecha' => 'required',
+            'venta_subtotal' => 'required',
+            'venta_iva' => 'required',
+            'venta_total' => 'required',
+            'venta_formapago' => 'required',
+            'venta_serie' => 'required',
+            'venta_tipo' => 'required',
+            'venta_metodo' => 'required',
+            'venta_moneda' => 'required',
+            //'venta_uso_cfdi' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Error al validar datos',
+                'errors' => $validator->errors(),
+                'status' => '400'
+            ], 400);
+        }
+
+        $venta = Venta::where('k_venta', $id)->where('k_empresa', $request->k_empresa)->first();
+        if ($venta) {
+            $venta->fill($request->all());
+            if ($venta->save()) {
+                return response()->json([
+                    'message' => 'Venta actualizada correctamente',
+                    'data' => $venta,
+                    'status' => '200'
+                ], 200);
+            }
+            return response()->json([
+                'message' => 'Error al actualizar venta',
+                'data' => $venta,
+                'status' => '400'
+            ], 400);
+        }
+        return response()->json([
+            'message' => 'Venta no encontrada',
+            'status' => '404'
+        ], 404);
+    }
+
+    //se estan borrando todos los registros con el mismo k_venta
+    public function delete($id, Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'k_empresa' => 'required',
+            'k_sujeto' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Error al validar datos',
+                'errors' => $validator->errors(),
+                'status' => '400'
+            ], 400);
+        }
+
+        $venta = Venta::where('k_venta', $id)->where('k_empresa', $request->k_empresa)->where('k_sujeto', $request->k_sujeto)->first();
+        if ($venta) {
+            if ($venta->delete()) {
+                return response()->json([
+                    'message' => 'Venta eliminada correctamente',
+                    'data' => $venta,
+                    'status' => '200'
+                ], 200);
+            }
+            return response()->json([
+                'message' => 'Error al eliminar venta',
+                'data' => $venta,
+                'status' => '400'
+            ], 400);
+        }
+        return response()->json([
+            'message' => 'Venta no encontrada',
+            'status' => '404'
+        ], 404);
+    }
 }
